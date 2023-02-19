92005cc9c83cdfebbe5d20b8557636d01e51984c
Mathilde Hetru <76809829+mathhetru@users.noreply.github.com>
2023-02-19 10:23:00 +0100

    WIP factory for one photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,28 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// import { photographerFactory } from "../factories/photographer.js";
+    
+async function getOneData() {
+    const requestURL = "http://localhost:5500/data/photographers.json"
+    return fetch(requestURL)
+    .then(response => response.json())
+    .catch(error => alert("Erreur : " + error));
+} 
+
+// Function affiche tous les photograhes
+async function displayPhotographers(data) {
+    // DOM Element - div contenant tous les articles
+    const photographersSection = document.querySelector(".photographer-section");
+
+    // Pour chaque photographe
+    data.photographers.forEach((photographer) => {
+        const photographerCard = photographerFactory(photographer);
+        const articlePanel = photographerCard.articlePanelDOM(photographerCard.name, photographerCard.id, photographerCard.city, photographerCard.country, photographerCard.tagline, photographerCard.price, photographerCard.picture);
+        photographersSection.appendChild(articlePanel);
+    });
+};
+
+async function init() {
+    const photographers = await getOneData();
+    displayPhotographers(photographers);
+};
+
+init(); 
